1c3b47b6e5596dcc1d530552715863d127d77d77
Alex <d3v.android@ya.ru>
2020-04-09 02:16:04 +0300

    - Refactoring models;

--- domain/src/main/java/com/geekbrains/team/domain/movies/model/Movie.kt
+++ domain/src/main/java/com/geekbrains/team/domain/movies/model/Movie.kt
@@ -1,19 +1,57 @@
 package com.geekbrains.team.domain.movies.model
 
 data class Movie(
-    val id: Int,
-    val title: String,
-    val originalTitle: String,
-    val popularity: Double,
-    val voteCount: Int,
-    val video: Boolean,
-    val trailer: String,
-    val posterPath: String?,
     val adult: Boolean,
-    val backdropPath: String?,
+    val backdropPath: String,
+    val belongsToCollection: Any?,
+    val budget: Int,
+    val genres: List<Genre>,
+    val id: Int,
+    val images: List<String>,
+    val trailer: String,
+    val homepage: String,
+    val imdbId: String,
     val originalLanguage: String,
-    val genreIds: List<Int>,
-    val voteAverage: Int,
+    val originalTitle: String,
     val overview: String,
-    val releaseDate: String
-)
+    val popularity: Double,
+    val posterPath: String,
+    val productionCompanies: List<ProductionCompany>,
+    val productionCountries: List<ProductionCountry>,
+    val releaseDate: String,
+    val revenue: Int,
+    val runtime: Int,
+    // TODO Add class for actors.
+    val cast: List<String>, // Актеры
+    // TODO Add class for crew
+    val crew: List<String>, // Команда
+    val spokenLanguages: List<SpokenLanguage>,
+    val status: String,
+    val tagline: String,
+    val title: String,
+    val video: Boolean,
+    val voteAverage: Double,
+    val voteCount: Int
+) {
+    data class Genre(
+        val id: Int,
+        val name: String
+    )
+
+    data class ProductionCompany(
+        val id: Int,
+        val logoPath: Any?,
+        val name: String,
+        val originCountry: String
+    )
+
+    data class ProductionCountry(
+        val iso31661: String,
+        val name: String
+    )
+
+    data class SpokenLanguage(
+        val iso6391: String,
+        val name: String
+    )
+}
--- domain/src/main/java/com/geekbrains/team/domain/tv/model/TVShow.kt
+++ domain/src/main/java/com/geekbrains/team/domain/tv/model/TVShow.kt
@@ -1,18 +1,87 @@
 package com.geekbrains.team.domain.tv.model
 
 data class TVShow(
-    val id: Int,
-    val originalName: String,
-    val name: String,
-    val popularity: Double,
-    val trailer: String,
-    val genreIds: List<Int>,
-    val overview: String,
-    val originCountry: List<String>,
-    val voteCount: Int,
     val backdropPath: String,
+    val createdBy: List<CreatedBy>,
+    val episodeRunTime: List<Int>,
+    val firstAirDate: String,
+    val genres: List<Genre>,
+    val homepage: String,
+    val id: Int,
+    val trailer: String,
+    val inProduction: Boolean,
+    val languages: List<String>,
+    val lastAirDate: String,
+    val lastEpisodeToAir: LastEpisodeToAir,
+    val name: String,
+    val networks: List<Network>,
+    val nextEpisodeToAir: Any?,
+    val numberOfEpisodes: Int,
+    val numberOfSeasons: Int,
+    val originCountry: List<String>,
     val originalLanguage: String,
-    val voteAverage: Int,
+    val originalName: String,
+    val overview: String,
+    val popularity: Double,
     val posterPath: String,
-    val firstAirDate: String
-)
+    val productionCompanies: List<ProductionCompany>,
+    val seasons: List<Season>,
+    val status: String,
+    val type: String,
+    val voteAverage: Double,
+    val voteCount: Int,
+    // TODO Add class for actors.
+    val cast: List<String>, // Актеры
+    val images: List<String>
+) {
+    data class CreatedBy(
+        val creditId: String,
+        val gender: Int,
+        val id: Int,
+        val name: String,
+        val profilePath: String
+    )
+
+    data class Genre(
+        val id: Int,
+        val name: String
+    )
+
+    data class LastEpisodeToAir(
+        val airDate: String,
+        val episodeNumber: Int,
+        val id: Int,
+        val name: String,
+        val overview: String,
+        val productionCode: String,
+        val seasonNumber: Int,
+        val showId: Int,
+        val stillPath: String,
+        val voteAverage: Double,
+        val voteCount: Int
+    )
+
+    data class Network(
+        val id: Int,
+        val logoPath: String,
+        val name: String,
+        val originCountry: String
+    )
+
+    data class ProductionCompany(
+        val id: Int,
+        val logoPath: String?,
+        val name: String,
+        val originCountry: String
+    )
+
+    data class Season(
+        val airDate: String,
+        val episodeCount: Int,
+        val id: Int,
+        val name: String,
+        val overview: String,
+        val posterPath: String,
+        val seasonNumber: Int
+    )
+}
