dc1ec0ee50a0d128cfb0abf4f47ceb045a1e6610
Mihail Dorofeev <crabgore.gonna.hate@gmail.com>
2020-04-09 11:26:06 +0200

    Опять переделал лэйауты под новые модели. Добавил адаптер для полной информации Добавил слушатель на кнопку запуска трейлера Заменил ViewPager на ViewPager2 чтобы нормально использовать RecyclerViewAdapter

--- ui/src/main/java/com/geekbrains/team/filmlibrary/adapters/FullInfoAdapter.java
+++ ui/src/main/java/com/geekbrains/team/filmlibrary/adapters/FullInfoAdapter.java
@@ -3,7 +3,6 @@ package com.geekbrains.team.filmlibrary.adapters;
 import android.content.Intent;
 import android.net.Uri;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -14,29 +13,24 @@ import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.geekbrains.team.filmlibrary.R;
-import com.geekbrains.team.filmlibrary.databinding.BigCardItemBinding;
+import com.geekbrains.team.filmlibrary.databinding.FullFilmInfoItemBinding;
 import com.geekbrains.team.filmlibrary.model.MovieView;
 import com.geekbrains.team.filmlibrary.model.TVShowView;
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
-import java.util.List;
+public class FullInfoAdapter extends RecyclerView.Adapter<FullInfoAdapter.ViewHolder> {
+    private MovieView movie;
+    private TVShowView tvShow;
 
-class BigCardAdapter extends RecyclerView.Adapter<BigCardAdapter.BigCardHolder> {
-    private List<MovieView> movie = new ArrayList<>();
-    private List<TVShowView> tvShow = new ArrayList<>();
-
-    public BigCardAdapter() {
+    public FullInfoAdapter() {
     }
 
-    public void setMovies(List<MovieView> data) {
-        movie.clear();
-        movie.addAll(data);
+    public void setMovie(MovieView data) {
+        movie = data;
     }
 
-    public void setTVShows(List<TVShowView> data) {
-        tvShow.clear();
-        tvShow.addAll(data);
+    public void setTvShow(TVShowView data) {
+        tvShow = data;
     }
 
     @BindingAdapter({"app:url"})
@@ -46,44 +40,51 @@ class BigCardAdapter extends RecyclerView.Adapter<BigCardAdapter.BigCardHolder> 
 
     @NonNull
     @Override
-    public BigCardHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        BigCardItemBinding binding = DataBindingUtil.inflate(inflater, R.layout.big_card_item, parent, false);
-        return new BigCardHolder(binding);
+        FullFilmInfoItemBinding binding = DataBindingUtil.inflate(inflater, R.layout.full_film_info_item, parent, false);
+        return new ViewHolder(binding);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull BigCardHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         String trailer;
-        if (!movie.isEmpty()) {
-            holder.bindMovie(movie.get(position));
-            trailer = movie.get(position).getTrailer();
+        String backDrop;
+
+        if (movie != null) {
+            holder.bindMovie(movie);
+            trailer = movie.getTrailer();
+            backDrop = movie.getImages().get(position);
         } else {
-            holder.bindTVShow(tvShow.get(position));
-            trailer = tvShow.get(position).getTrailer();
+            holder.bindTVShow(tvShow);
+            trailer = tvShow.getTrailer();
+            backDrop = tvShow.getImages().get(position);
         }
 
         String finalTrailer = trailer;
         holder.play.setOnClickListener(v -> v.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(finalTrailer))));
+        Picasso.get().load(backDrop).into(holder.backDrop);
     }
 
     @Override
     public int getItemCount() {
-        if (!movie.isEmpty())
-            return movie.size();
+        if (movie != null)
+            return movie.getImages().size();
         else
-            return tvShow.size();
+            return tvShow.getImages().size();
     }
 
-    class BigCardHolder extends RecyclerView.ViewHolder {
+    class ViewHolder extends RecyclerView.ViewHolder {
         private ImageButton play;
+        private ImageView backDrop;
 
-        BigCardItemBinding binding;
+        FullFilmInfoItemBinding binding;
 
-        BigCardHolder(BigCardItemBinding binding) {
+        ViewHolder(FullFilmInfoItemBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
             play = binding.getRoot().findViewById(R.id.play_btn);
+            backDrop = binding.getRoot().findViewById(R.id.title_iv);
         }
 
         void bindMovie(MovieView movie) {
